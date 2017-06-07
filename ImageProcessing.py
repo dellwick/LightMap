bfc6ff64e9fd958670b139bb0679e96414f9b2a9
zac <zac.freer@knights.ucf.edu>
2017-06-06 23:58:43 -0400

    Finished adding support for unanimated images

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -27,7 +27,7 @@ class ImageProcessing():
         info = info.split('/')
         
         if info[0] != 'image':
-            raise IOError("Not an image")
+            raise TypeError("Application doesn't support this file format")
         
         elif info[1] not in animated_formats:
             self.nonanimated_image()
@@ -37,59 +37,57 @@ class ImageProcessing():
     
     # Handles all non animated file formats        
     def nonanimated_image(self):
-        image = cv2.imread(self.filename, -1)
-        #image = self._add_alpha(image)
-        
-        
+        image = cv2.imread(self.filename, 1)
         
         self.generate_circle(image)
-        
-        
-        
-        '''
-        mask = np.zeros(image.shape, dtype=np.uint8)
-        
-        
-        roi_corners = np.array([[(10,400),(300,400),(10,300)]], dtype=np.int32)
-        
-        channel_count = image.shape[2]
-        ignore_mask_color = (255, )*channel_count
-        
-        cv2.fillPoly(mask, roi_corners, ignore_mask_color)
-        
-        masked_image = cv2.bitwise_and(image, mask)
-        
-        cv2.imwrite('output.png', masked_image)
-        '''
-        
-    
-        
+            
+    # Creates circular image with a transparent background
     def generate_circle(self, image):
-        self.crop(image)
+        image = self.crop_circle(image)
+        image = self.add_alpha(image)
+        
+        cv2.imwrite("output.png", image)
+        return image
         
     
-    # Crops image into square with the dimensions 2*radius-X-2*radius    
-    def crop(self, image):
+    # outlines the largest, center circle in the image  
+    def crop_circle(self, image):
         self.get_size(image)
         index = self.find_min(self.size[0], self.size[1])
         
         radius = floor(self.size[index] / 2)
         center = (floor(self.size[0] / 2), floor(self.size[1] / 2))
-       
-        circle = image[(center[0] - radius):(center[0] + radius),
-                       (center[1] - radius):(center[1] + radius)]
         
-        self.add_alpha(image, circle, radius, center)
+        
+        circle = np.zeros(self.size, np.uint8)
+        
+        cv2.circle(circle, (center[1], center[0]), radius, 1, thickness=-1)
+        
+        image = cv2.bitwise_and(image, image, mask=circle)
+        
+        image = self.trim_square(image, center, radius)
+        
+        cv2.imwrite("circle.png", image)
+        return image
+        
+    # Crops the square surrounding the largest, centermost circle of the image 
+    def trim_square(self, image, center, radius):
+        image = image[(center[0] - radius):(center[0] + radius),
+                      (center[1] - radius):(center[1] + radius)]
+    
+        return image
+        
         
     # Adds alpha layer behind circle image
-    def add_alpha(self, image, circle, radius, center):
-        b_channel, g_channel, r_channel = cv2.split(image)
+    def add_alpha(self, image):
+        greyscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        _, alpha = cv2.threshold(greyscale, 0, 255, cv2.THRESH_BINARY)
+        b, g, r = cv2.split(image)
         
-        a_channel = np.ones(b_channel.shape, dtype=b_channel.dtype) * 50
+        fourlayer = [b, g, r, alpha]
+        return cv2.merge(fourlayer, 4)
         
         
-        
-    
     # Returns the index of the shorter dimension of the input image
     def find_min(self, x, y):
         if x < y:
@@ -102,9 +100,6 @@ class ImageProcessing():
         self.size = image.shape[:2]
         
         
-        
-        
-        
     # Handles animated image formats 
     def animated_image(self):
-        return
+        raise TypeError("Application doesn't support this file format")
